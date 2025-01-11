d1bac96b97e33e16ae92b0a458feca27755e0aa2
avacado418 <avikagarwal10@gmail.com>
2025-01-11 10:35:04 -0800

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,6 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.Pose2d;
+`import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
@@ -8,7 +8,7 @@ import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;`
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
@@ -101,7 +101,7 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myFourthBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(10, 62, Math.toRadians(-90)))
+        myFourthBot.runAction(myThirdBot.getDrive().actionBuilder(new Pose2d(10, 62, Math.toRadians(-90)))
                 .lineToY(35)
                 .setReversed(true)
                 .splineTo(new Vector2d(48,34), Math.toRadians(-90))
@@ -115,6 +115,8 @@ public class MeepMeepTesting {
                 .setReversed(true)
                 .lineToX(25)
                 .build());
+
+
 
         RoadRunnerBotEntity myFifthBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
