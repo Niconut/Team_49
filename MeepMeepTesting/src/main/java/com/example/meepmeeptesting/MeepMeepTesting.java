bbb72ca1ea34875a462075549c96a310cff7371a
jam687 <109048226+jam687@users.noreply.github.com>
2025-01-11 14:35:33 -0800

    Added Push Auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,6 @@
 package com.example.meepmeeptesting;
 
-`import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
@@ -8,7 +8,7 @@ import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;`
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
@@ -125,32 +125,24 @@ public class MeepMeepTesting {
                 .build();
 
         myFifthBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(10, -63.5, Math.toRadians(90)))
-                .lineToY(-35)
-                .strafeToLinearHeading(new Vector2d(36,-60), Math.toRadians(90))
-                .strafeToLinearHeading(new Vector2d(10,-34), Math.toRadians(90))
-                .strafeToLinearHeading(new Vector2d(36,-60), Math.toRadians(90))
+                .lineToY(-34)
+                .setReversed(true)
+                .strafeToLinearHeading(new Vector2d(9.5,-60), Math.toRadians(90))
+                .splineTo(new Vector2d(47,-15), Math.toRadians(-90))
+                .strafeToLinearHeading(new Vector2d(47,-63), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(47,-12), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(56,-12), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(58,-63), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(56,-12), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(61.75,-12), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(61.75,-60), Math.toRadians(90))
                 .build());
 
-        RoadRunnerBotEntity mySixthBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setColorScheme(new ColorSchemeBlueLight())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-
-        mySixthBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-10, 63.5, Math.toRadians(-90)))
-                .lineToY(35)
-                .strafeToLinearHeading(new Vector2d(-36,60), Math.toRadians(-90))
-                .strafeToLinearHeading(new Vector2d(-10,34), Math.toRadians(-90))
-                .strafeToLinearHeading(new Vector2d(-36,60), Math.toRadians(-90))
-                .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .addEntity(mySecondBot)
-                .addEntity(myThirdBot)
-                .addEntity(myFourthBot)
+                .addEntity(myFifthBot)
                 .start();
 
 
