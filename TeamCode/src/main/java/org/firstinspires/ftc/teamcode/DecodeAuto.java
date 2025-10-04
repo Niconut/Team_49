54ea0f73139f765c012b10792a0915a1a53c44da
Niconut <140370929+Niconut@users.noreply.github.com>
2025-10-03 20:41:27 -0700

    Added New Auto + Subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeAuto.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public final class DecodeAuto extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose =
+    }
+}
